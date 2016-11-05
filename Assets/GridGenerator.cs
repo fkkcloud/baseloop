1ff8069ab43af693ea2a7115cef59d09a92a74b2
System Administrator <root@Jaes-MacBook.local>
2016-11-05 15:25:16 -0700

    sec

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour {
+
+	public GameObject Prefab;
+
+	public int x = 8;
+	public int y = 8;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				Quaternion quat = Quaternion.Euler (new Vector3 (0f, 0f, 0f));
+				Instantiate(Prefab, new Vector3 (i, j, 0f), quat); 
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
